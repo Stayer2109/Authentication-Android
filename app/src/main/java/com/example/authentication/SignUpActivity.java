6783a910920ecdec0329d2f01b68a664e1db6357
quangtrandinhminh <trandinhminhquangsxp@gmail.com>
2024-10-11 03:07:51 +0700

    Add firebase & signup function

--- app/src/main/java/com/example/authentication/SignUpActivity.java
+++ app/src/main/java/com/example/authentication/SignUpActivity.java
@@ -2,7 +2,10 @@ package com.example.authentication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends AppCompatActivity {
     // Variables
     TextView clickableSignIn;
+    EditText etEmail, etPassword, etPhone;
+    Button btnRegister;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +35,65 @@ public class SignUpActivity extends AppCompatActivity {
 
         // Mappings
         clickableSignIn = findViewById(R.id.clickableSignIn);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        etPhone = findViewById(R.id.etPhone);
+        btnRegister = findViewById(R.id.btnRegister);
 
         // Sign In Text Click Event
         clickableSignIn.setOnClickListener(v -> {
             startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
             finish();
         });
+
+        // Sign Up Button Click Event
+        findViewById(R.id.btnRegister).setOnClickListener(v -> {
+            String email = etEmail.getText().toString();
+            String password = etPassword.getText().toString();
+            if (!checkInputFields(email, password)) {
+                return;
+            }
+
+            // Sign Up Logic
+            signUp(email, password);
+        });
+    }
+
+    // check input fields
+    private boolean checkInputFields(String email, String password) {
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please fill email and password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Check if email is valid
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            Toast.makeText(this, "Please enter a valid email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        // Firebase Auth requires password to be at least 6 characters
+        if (password.length() < 6) {
+            Toast.makeText(this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+
+    // Use Firebase Auth to Sign Up
+    private void signUp(String email, String password) {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, task -> {
+                        if (task.isSuccessful()) {
+                            // Sign Up Success
+                            startActivity(new Intent(SignUpActivity.this, SignInActivity.class));
+                            Toast.makeText(SignUpActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            // Sign Up Failed, show error message from Firebase
+                            Toast.makeText(SignUpActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
     }
 }
